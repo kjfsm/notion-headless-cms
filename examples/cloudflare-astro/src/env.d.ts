510c25c5ef2acf1face21c755d2c632197487727
kjfsm <kjfsm@users.noreply.github.com>
2026-04-21 18:23:21 +0900

    update

--- examples/cloudflare-astro/src/env.d.ts
+++ examples/cloudflare-astro/src/env.d.ts
@@ -1,11 +1,4 @@
-/// <reference types="astro/client" />
-/// <reference types="@cloudflare/workers-types" />
-
-type Runtime = import("@astrojs/cloudflare").Runtime<{
-	NOTION_TOKEN: string;
-	NOTION_DATA_SOURCE_ID: string;
-	CACHE_BUCKET?: R2Bucket;
-}>;
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
 	interface Locals extends Runtime {}
