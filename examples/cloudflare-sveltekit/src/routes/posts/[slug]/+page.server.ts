616c6faa678e3843f79f6bef7366cf500027f97f
kjfsm <kjfsm@users.noreply.github.com>
2026-04-23 16:35:13 +0900

    format

--- examples/cloudflare-sveltekit/src/routes/posts/[slug]/+page.server.ts
+++ examples/cloudflare-sveltekit/src/routes/posts/[slug]/+page.server.ts
@@ -3,7 +3,13 @@ import { createCMS } from "$lib/cms";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, platform }) => {
-	const cms = createCMS(platform!.env);
+	if (!params.slug) {
+		error(404, "Not Found");
+	}
+	if (!platform) {
+		error(500, "Platform not found");
+	}
+	const cms = createCMS(platform.env);
 	const post = await cms.posts.getItem(params.slug);
 	if (!post) error(404, "Not Found");
 	const html = await post.content.html();
