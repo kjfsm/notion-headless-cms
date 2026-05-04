4d215687e379325219921e47427c287f57ea2a3f
kjfsm <kjfsm@users.noreply.github.com>
2026-05-04 17:47:36 +0900

    fix

--- examples/cloudflare-react-router/app/routes/images.ts
+++ examples/cloudflare-react-router/app/routes/images.ts
@@ -3,7 +3,7 @@ import type { Route } from "./+types/images";
 
 export async function loader({ params, context }: Route.LoaderArgs) {
   const cms = makeCms(context.cloudflare.env);
-  const object = await cms.$getCachedImage(params.hash ?? "");
+  const object = await cms.getCachedImage(params.hash ?? "");
   if (!object) return new Response("Not Found", { status: 404 });
   const headers = new Headers();
   if (object.contentType) headers.set("content-type", object.contentType);
